c90fee926b1add1d3309c7dd42466c47c9dca06a
Student <NongPhloeut>
2022-11-29 10:17:02 +0700

    crate function search product

--- js/home.js
+++ js/home.js
@@ -1,8 +1,9 @@
-//// dropdown function
+
+  //=======================================dropdown function========================
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
-  
   // Close the dropdown if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
@@ -16,3 +17,35 @@ function myFunction() {
       }
     }
   }
+
+
+
+
+  //=======================================search products========================
+
+function searchProduct(){
+  const textInput = searchProductInput.value.toUpperCase();
+  const baner = document.querySelector(".baner");
+  const productList = document.querySelectorAll(".card-product");
+  const productName = document.getElementsByTagName("p");
+  for(let i = 0 ;i< productName.length ; i++){
+    let matches = productList[i].getElementsByTagName("p")[0];
+    if(matches){
+      let textValue = matches.textContent || matches.innerHTML;
+      if(textValue.toUpperCase().indexOf(textInput)> -1){
+        productList[i].style.display = "";
+      }
+      else{
+        baner.style.display = "none";
+        productList[i].style.display = "none";
+      }
+    }
+  }
+
+
+}
+  //=======================================MAIN===================================
+
+let searchProductInput = document.querySelector("#searcProduct");
+console.log(searchProductInput.value);
+searchProductInput.addEventListener("keyup" ,searchProduct);
